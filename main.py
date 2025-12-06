9358bd7d726afb87c82a3578c015f6cffd1cc488
sofia.hosorio <sofia.hosorio@udea.edu.co>
2025-12-06 12:39:17 -0500

    comentarios del main

--- main.py
+++ main.py
@@ -9,24 +9,24 @@ from Controlador import *
 def main():
     app = QApplication(sys.argv)
 
-    # Modelos "globales"
+# Modelos "globales"
     auth = AuthManager()
     logger = DatabaseLogger()
 
-    # ----- LOGIN -----
+# login
     login_view = LoginWindow()
     login_ctrl = LoginController(login_view, auth)
     login_view.controller = login_ctrl
 
-    # Ejecutar login como diálogo
+# Ejecutar login como diálogo
     result = login_view.exec_()
 
     if result == QDialog.Accepted:
-        # Usuario que inició sesión
+# Usuario que inició sesión
         username, _ = login_view.get_credentials()
         session = UserSession(username)
 
-        # ----- MAIN WINDOW -----
+# Main Window
         main_view = MainWindow()
 
         models_for_main = {
@@ -36,34 +36,34 @@ def main():
         main_ctrl = MainController(main_view, models_for_main)
         main_view.controller = main_ctrl
 
-        # ----- MODELOS ESPECÍFICOS -----
+# Modelos específicos
         image_model = ImageProcessor()
         signal_model = SignalProcessor()
         tabular_model = TabularProcessor()
 
-        # ----- VISTAS DE LOS TABS -----
+# Vistas específicas
         image_widget = ImageWidget()
         signal_widget = SignalWidget()
         tabular_widget = TabularWidget()
 
-        # ----- CONTROLADORES DE LOS TABS -----
+#controladores específicos
         image_ctrl = ImageController(image_widget, image_model)
         signal_ctrl = SignalController(signal_widget, signal_model)
         tabular_ctrl = TabularController(tabular_widget, tabular_model)
 
-        # Conectar vistas con sus controladores
+# Conectar vistas con sus controladores
         image_widget.controller = image_ctrl
         signal_widget.controller = signal_ctrl
         tabular_widget.controller = tabular_ctrl
 
-        # Poner los widgets en las pestañas del main
+# Poner los widgets en las pestañas del main
         main_view.set_tabs(image_widget, signal_widget, tabular_widget)
 
-        # Mostrar ventana principal
+# Mostrar ventana principal
         main_view.show()
         sys.exit(app.exec_())
     else:
-        # Si cierra el login sin autenticarse
+# Por si el usuario cierra el login sin autenticarse
         sys.exit(0)
 
 
